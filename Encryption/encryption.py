c83fe77ece5c500a9a09638959f5fa69d4a43e4a
Kporrecca <kporrecca@gmail.com>
2021-04-22 18:58:39 -0400

    commenting

--- Encryption/encryption.py
+++ Encryption/encryption.py
@@ -1,4 +1,4 @@
-#given a message m, using public key e and N
+#given a message m, using public key e and N return the encrypted ciphertext
 def encrypt(m, e, N):
     msg = [ord(c) for c in m]
     cphr = []
@@ -7,6 +7,7 @@ def encrypt(m, e, N):
         cphr.append(x)
     return cphr
 
+#given a message m, using the private key d and N return the decrypted plaintext
 def decrypt(m, d, N):
     pln = []
     for i in m:
@@ -14,3 +15,5 @@ def decrypt(m, d, N):
         pln.append(chr(x))
 
     return ''.join(pln)
+
+
