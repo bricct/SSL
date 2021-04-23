a0f984f66159a067f1ba5775fbb942c49380ad1f
Kporrecca <kporrecca@gmail.com>
2021-04-22 20:29:59 -0400

    final encrypt fix hopefully

--- src/Encryption/encryption.py
+++ src/Encryption/encryption.py
@@ -4,19 +4,17 @@ def encrypt(m, e, N):
     msg = [ord(c) for c in m]
     cphr = []
     for i in msg:
-        x = (int(i)**e) % N
+        x = pow(int(i),e, N)
         cphr.append(str(x))
     return '||'.join(cphr)
 
 #given a message m, using the private key d and N return the decrypted plaintext
 def decrypt(m, d, N):
-    m_list = m.split('||')
-    msg = [ord(c) for c in m_list]
+    msg = m.split('||')
     pln = []
     for i in msg:
-        x = (int(i)**d) % N
+        x = pow(int(i),d, N)
         pln.append(chr(x))
     return ''.join(pln)
-
 
 
