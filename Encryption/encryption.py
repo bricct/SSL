2e2be32dc5213f3e2fb2431bd71801e0f53ea4f1
Kporrecca <kporrecca@gmail.com>
2021-04-22 20:23:22 -0400

    encryption fix part 2

--- Encryption/encryption.py
+++ Encryption/encryption.py
@@ -1,22 +1,18 @@
-
 #given a message m, using public key e and N return the encrypted ciphertext
 def encrypt(m, e, N):
     msg = [ord(c) for c in m]
     cphr = []
     for i in msg:
-        x = (int(i)**e) % N
-        cphr.append(str(x))
-    return '||'.join(cphr)
+        x = pow(int(i),e, N)
+        cphr.append(chr(x))
+    return ''.join(cphr)
 
 #given a message m, using the private key d and N return the decrypted plaintext
 def decrypt(m, d, N):
-    m_list = m.split('||')
-    msg = [ord(c) for c in m_list]
     pln = []
-    for i in msg:
-        x = (int(i)**d) % N
+    for i in m:
+        x = pow(int(i), d, N)
         pln.append(chr(x))
+
     return ''.join(pln)
-
-
 
